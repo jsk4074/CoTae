71d259a3f43cc1c863e34ebb1c515693cfef8168
jsk4074 <jsk4074@naver.com>
2023-03-14 19:01:48 +0900

    CoTae

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,11 @@
+echo "------------------"
 echo "Git push is ready!"
- 
+echo "------------------"
+
 git add * 
 git commit -m "CoTae"
 git push
 
+echo "-----------------------------------"
 echo "File has been commited successfully!" 
+echo "-----------------------------------"
